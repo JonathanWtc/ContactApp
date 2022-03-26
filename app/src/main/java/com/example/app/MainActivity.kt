7741b65c94d5ca2577bedd98ee5051bfb3f665b3
Jonathan Trocones Collado <jonathanwtc16@gmail.com>
2022-03-26 00:07:29 -0500

    Se dio funcion al floatingButton para pasar al activity_add_contact

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var toAdapt : ContactsAdapter
+    private val myRequestCode = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         toAdapt = ContactsAdapter(ContactList.list)
         binding.recyclerContact.adapter = toAdapt
+
+        binding.btnFloatingAdd.setOnClickListener(){
+            startActivityForResult(Intent(this, ActivityAddContact::class.java),myRequestCode)
+        }
     }
 }
