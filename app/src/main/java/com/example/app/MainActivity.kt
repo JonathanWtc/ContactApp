b902e0cf27852f2efb9d5efac6ab426b848842bd
Jonathan Trocones Collado <jonathanwtc16@gmail.com>
2022-04-07 11:24:47 -0500

    Se inflan las vistas con el adaptador

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toAdapt: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toAdapt = MyAdapter(this, NameList.names)
+        binding.myRecyclerView.adapter = toAdapt
     }
 }
